5aa1939cac556522c22f0931e23dfe17954351fe
tang navy <1794758142@qq.com>
2025-03-27 10:42:49 +0800

    Update 12-成员变量和成员函数分开存储.cpp

--- 03-C++核心编程/12-成员变量和成员函数分开存储.cpp
+++ 03-C++核心编程/12-成员变量和成员函数分开存储.cpp
@@ -3,7 +3,7 @@
 //using namespace std;
 //class Bird
 //{
-//	//����һ������
+//	//定义一个空类
 //};
 //class Duck
 //{
@@ -13,8 +13,8 @@
 //class Worm
 //{
 //	int m_age;
-//	static int m_len;//��̬��Ա�����ͺ�����������������ϣ�
-//	void fun12()//�Ǿ�̬��Ա������Ҳ��������Ķ����ϣ�
+//	static int m_len;//静态成员变量和静态函数都不属于类对象上；
+//	void fun12()//非静态成员函数，也不属于类的对象上；
 //	{
 //
 //	}
@@ -25,14 +25,14 @@
 //int main() 
 //{
 //	Bird b1;
-//	//Ϊ�����ֲ�ͬ�Ŀն�������Ϊÿ���ն���Ҳ�÷���һ���ֽڣ�
-//	//������󻯵Ķ�����ǿն���û�г�ʼ���Ķ���ɲ��ǿն���
+//	//为了区分不同的空对象，于是为每个空对象也得分配一个字节；
+//	//空类具象化的对象才是空对象，没有初始化的对象可不是空对象；
 //	Duck d1;
-//	//�ǿն����Сֻ��Ǿ�̬��Ա�����йأ���Ա����������̬��Ա��������������ĳ�����󻯵Ķ���
+//	//非空对象大小只与非静态成员变量有关，成员函数包括静态成员函数，都不属于某个具象化的对象；
 //	Worm w1;
-//	cout << "�ն���b1��СΪ��" << sizeof(b1) << endl;
-//	cout << "����d1��СΪ��" << sizeof(d1) << endl;
-//	cout << "����w1��СΪ��" << sizeof(w1) << endl;
+//	cout << "空对象b1大小为：" << sizeof(b1) << endl;
+//	cout << "对象d1大小为：" << sizeof(d1) << endl;
+//	cout << "对象w1大小为：" << sizeof(w1) << endl;
 //	system("pause");
 //	return 0;
 //}
